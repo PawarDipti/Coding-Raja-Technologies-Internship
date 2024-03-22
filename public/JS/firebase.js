b2b7dbf89494d9012657e0304eb6a045fd4cc62c
pradip pawar <diptipawar5005@gmail.com>
2024-03-22 03:16:12 -0500

    Sample commit

---
+++ public/JS/firebase.js
@@ -0,0 +1,25 @@
+let firebaseConfig = {
+
+    apiKey: "AIzaSyAhBlFOCuE-K_e9fx3w2JWQPy_zRpZ1Ryg",
+
+    authDomain: "fir-ab6ef.firebaseapp.com",
+
+    projectId: "fir-ab6ef",
+
+    storageBucket: "fir-ab6ef.appspot.com",
+
+    messagingSenderId: "322299801933",
+
+    appId: "1:322299801933:web:254380ea814c7174bb4704"
+
+  };
+
+
+  // Initialize Firebase
+
+  firebase.initializeApp(firebaseConfig);
+
+
+  let db = firebase.firestore();
+
+  
